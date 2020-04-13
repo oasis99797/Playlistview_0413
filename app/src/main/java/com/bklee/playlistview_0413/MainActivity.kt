94522a59130db642f3ffcdcb1b240bb36b3cc1f7
oasis99797 <oasis99797@gmail.com>
2020-04-13 22:22:35 +0900

    실제 데이터 반영

--- app/src/main/java/com/bklee/playlistview_0413/MainActivity.kt
+++ app/src/main/java/com/bklee/playlistview_0413/MainActivity.kt
@@ -2,11 +2,32 @@ package com.bklee.playlistview_0413
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import com.bklee.playlistview_0413.adapters.AppAdapter
+import com.bklee.playlistview_0413.datas.App
+
 
 class MainActivity : AppCompatActivity() {
+
+    var mAppAdapter: AppAdapter? = null
+    val apps = ArrayList<App>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        apps.add(App("아스팔트 8 : 에어본", "GameLoft", 5))
+        apps.add(App("MineCraft", "Mojang", 4))
+        apps.add(App("아스팔트 7 : 히트", "GameLoft", 4))
+        apps.add(App("팔라독 (Paladog)", "FazeCat", 2))
+        apps.add(App("Plants vs. Zombies", "EA Games", 3))
+        apps.add(App("스왐피 (Swampy)", "Disney", 5))
+        apps.add(App("리니지2M", "NCSOFT", 3))
+        apps.add(App("리니지M", "NCSOFT", 4))
+        apps.add(App("AFK아레나", "Lilith Games", 1))
+
+        mAppAdapter = AppAdapter(this, R.layout.app_list_item, apps)
+        appListView.adapter = mAppAdapter
+
     }
 }
--- app/src/main/java/com/bklee/playlistview_0413/adapters/AppAdapter.kt
+++ app/src/main/java/com/bklee/playlistview_0413/adapters/AppAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.bklee.playlistview_0413.R
 import com.bklee.playlistview_0413.datas.App
 
@@ -24,6 +26,42 @@ class AppAdapter(context: Context, resId: Int, list:ArrayList<App>) : ArrayAdapt
         }
 
         val row = tempRow!!
+
+
+        val titleTxt = row.findViewById<TextView>(R.id.titleTxt)
+        val companyNameTxt = row.findViewById<TextView>(R.id.companyNameTxt)
+        val starImg1 = row.findViewById<ImageView>(R.id.starImg1)
+        val starImg2 = row.findViewById<ImageView>(R.id.starImg2)
+        val starImg3 = row.findViewById<ImageView>(R.id.starImg3)
+        val starImg4 = row.findViewById<ImageView>(R.id.starImg4)
+        val starImg5 = row.findViewById<ImageView>(R.id.starImg5)
+
+        val data = mList.get(position)
+
+        titleTxt.text = "${position+1}. ${data.name}"
+        companyNameTxt.text = data.companyName
+
+//        starImg1.setImageResource(R.drawable.fill_star)
+
+        val starImgList = ArrayList<ImageView>()
+        starImgList.add(starImg1)
+        starImgList.add(starImg2)
+        starImgList.add(starImg3)
+        starImgList.add(starImg4)
+        starImgList.add(starImg5)
+
+        for (index in 0..4) {
+            val starImg = starImgList.get(index)
+
+            if (index < data.starCount) {
+                starImg.setImageResource(R.drawable.fill_star)
+            }
+            else {
+                starImg.setImageResource(R.drawable.empty_star)
+            }
+
+        }
+
 
 
         return row
